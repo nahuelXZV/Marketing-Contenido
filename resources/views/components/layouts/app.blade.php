1fc27e28e5c15cc07e982d0e7c7b113cf6574896
Nahuel Zalazar <zvnahuel63@gmail.com>
2024-04-06 15:37:34 -0400

    change(layout): Change title layout

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" type="image/png" href="{{ asset('imgs/logo.png') }}">
-    <title>EI-SISTEMA</title>
+    <title>Marketing</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
