95d70f8ef769def738d44589380f65b09225829e
Nahuel Zalazar <zvnahuel63@gmail.com>
2024-04-30 14:25:46 -0400

    add(migrations): Add migrations ands models

---
+++ database/migrations/2024_04_30_181911_create_campaigns_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('campaigns', function (Blueprint $table) {
+            $table->id();
+            $table->string('codigo')->nullable();
+            $table->string('tematica');
+            $table->text('descripcion');
+            $table->string('fecha_inicio');
+            $table->string('fecha_final');
+            $table->string('estado')->default('Borrador');
+            $table->string('invervalo')->nullable();
+            $table->string('audiencia');
+            $table->string('presupuesto')->nullable();
+            $table->string('objetivo')->nullable();
+
+            $table->unsignedBigInteger('company_id');
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('campaigns');
+    }
+};
---
+++ database/migrations/2024_04_30_181931_create_publications_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('publications', function (Blueprint $table) {
+            $table->id();
+            $table->string('codigo')->nullable();
+            $table->string('titulo');
+            $table->text('contenido');
+            $table->text('descripcion_recurso');
+            $table->string('fecha_publicacion')->nullable();
+            $table->string('hora_publicacion')->nullable();
+            $table->string('presupuesto')->nullable();
+            $table->string('estado')->default('Borrador');
+
+            $table->unsignedBigInteger('campaign_id');
+            $table->foreign('campaign_id')->references('id')->on('campaigns')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('publications');
+    }
+};
