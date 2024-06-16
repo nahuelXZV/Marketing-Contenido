05fe3ffef0b4c3fefb109b537fce34ecda44bffd
Nahuel Zalazar <zvnahuel63@gmail.com>
2024-06-16 15:05:58 -0400

    fix(aside) Fix aside

--- resources/views/components/shared/aside.blade.php
+++ resources/views/components/shared/aside.blade.php
@@ -92,13 +92,6 @@
                     <span class="ms-3">Campañas</span>
                 </a>
             </li>
-            <li>
-                <a href="{{ route('user.list') }}"
-                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700 group">
-                    <x-icons.megaphone />
-                    <span class="ms-3">Redes sociales</span>
-                </a>
-            </li>
             @can('company')
                 <li>
                     <a href="{{ route('company.edit', auth()->user()->company_id) }}"
